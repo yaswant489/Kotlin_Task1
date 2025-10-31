fb996b2339d2f8a33f8cfcfb83d5d5d54373c9e7
yaswant poudel <yaswantpoudel0@gmail.com>
2025-10-31 19:27:18 +0545

    Assignment

---
+++ app/src/main/java/com/example/ai37c/assignment/KotlinCollections.kt
@@ -0,0 +1,13 @@
+package com.example.ai37c.assignment
+
+fun main(){
+    val wordsMeanings = mapOf(
+        "cause" to "the reason why something, especially something bad, happens",
+        "assignment" to "a task or piece of work allocated to someone as part of a job or course of study.",
+        "issue" to "an important topic or problem for debate or discussion.",
+        "problem" to "a matter or situation regarded as unwelcome or harmful and needing to be dealt with and overcome.",
+        "male" to "of or denoting the sex that produces gametes, especially spermatozoa, with which a female may be fertilized or inseminated to produce offspring.",
+    )
+    println("Enter a word to find its meaning: ")
+    val input : String = readln().lowercase()
+    println(wordsMeanings[input])
---
+++ app/src/main/java/com/example/ai37c/day1.kt
@@ -0,0 +1,23 @@
+package com.example.ai37c
+
+import android.R
+import androidx.compose.animation.shrinkOut
+
+fun main(){
+    println("Hello")
+    println("Im under the water")
+
+    var name : String = "Yaswant"
+    val age : Int = 18
+    val terms : Boolean = false
+    val weight : Double = 0.0
+
+
+    print("my name is ${name.uppercase()} and age is $age . Save me !!!")
+
+    // range to = ..
+    //until = <
+    // down to = >
+
+
+}
